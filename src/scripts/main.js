4c18d471db31d1a2b3141599b03097539f4d1d66
Krzysztof Budnik <budnix@gmail.com>
2015-04-23 15:35:25 +0200

    Change 'rows' to 'cells'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -723,7 +723,7 @@ var HT = (function () {
             afterLoadData: function () {
               setTimeout(function () {
                 hotInstances['index'].selectCell(9999, 0);
-                loadDataButton.innerHTML = "100 000 rows loaded";
+                loadDataButton.innerHTML = "100 000 cells loaded";
               }, 250)
             }
           });
